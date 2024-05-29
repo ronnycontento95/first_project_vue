da8cf731e58d95ffbe8b9fe174441f0c24d5a079
Ronny Contento <ronnyfer95@gmail.com>
2024-05-29 12:36:06 -0500

    Install boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,49 @@
 <script setup>
-import { ref } from 'vue';
-import BlogPost from './components/BlogPost.vue';
+import { ref } from "vue";
+import BlogPost from "./components/BlogPost.vue";
 
 const postsData = ref([
   {
     id: 1,
     title: "Post 1",
-    body: "Description 1"
+    body: "Description 1",
   },
   {
     id: 2,
     title: "Post 2",
-    body: "Description 2"
+    body: "Description 2",
   },
   {
     id: 3,
     title: "Post 3",
-    body: "Description 3"
+    body: "Description 3",
   },
   {
     id: 4,
     title: "Post 4",
-    body: "Description 4"
-  }
+    body: "Description 4",
+  },
 ]);
 
+const favoritoref = ref("");
+
+const fijarFavorito = (title) => {
+  favoritoref.value = title;
+};
 </script>
 
 <template>
   <div class="container">
     <h1>APP RONNY</h1>
-    <BlogPost v-for="postsdata in postsData" :key="postsdata.id" :title="postsdata.title" :body="postsdata.body" :id="postsdata.id"/>
+    <h2>MI FAVORTITE IS {{ favoritoref }}</h2>
+    <BlogPost
+      v-for="post in postsData"
+      :key="post.title"
+      :title="post.title"
+      :id="post.id"
+      :body="post.body"
+      class="mb-2"
+      :fijarFavorito="fijarFavorito"
+    />
   </div>
 </template>
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,25 +1,26 @@
 <script setup>
-// const props = defineProps(["title", "id", "body", "]);
-
 defineProps({
     title: String,
     id: Number,
-    colorText: String,
     body: {
-        type:String,
-        default: "Sin descripcion",
-    }
+        type: String,
+        default: "Sin descripción",
+    },
+    fijarFavorito: Function,
 });
 
-
+// defineProps(["title", "id", "body", "fijarFavorito"]);
 </script>
 
 <template>
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">{{ title }}</h5>
-            <p>{{ id }} {{ body }}</p>
-            <button class="btn btn-sm btn-outline-primary" @click="fijarFavorito(title)">
+            <p>{{ body }}</p>
+            <button
+                class="btn btn-sm btn-outline-primary"
+                @click="fijarFavorito(title)"
+            >
                 Mi Favorito
             </button>
         </div>
