e43bc80f8e262e12c15dbd1097818be862d9b55b
Ronny Contento <ronnyfer95@gmail.com>
2024-05-29 10:19:57 -0500

    Creacion de mi primer componente con un v-for

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+import BlogPost from './components/BlogPost.vue';
+
+const postsData = ref([
+  {
+    id: 1,
+    title: "Post 1",
+    body: "Description 1"
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    body: "Description 2"
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    body: "Description 3"
+  },
+  {
+    id: 4,
+    title: "Post 4",
+    body: "Description 4"
+  }
+]);
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Ronny Development Vue" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>APP RONNY</h1>
+    <BlogPost v-for="postsdata in postsData" :key="postsdata.id" :title="postsdata.title" :body="postsdata.body" :id="postsdata.id"/>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,27 @@
+<script setup>
+// const props = defineProps(["title", "id", "body", "]);
+
+defineProps({
+    title: String,
+    id: Number,
+    colorText: String,
+    body: {
+        type:String,
+        default: "Sin descripcion",
+    }
+});
+
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p>{{ id }} {{ body }}</p>
+            <button class="btn btn-sm btn-outline-primary" @click="fijarFavorito(title)">
+                Mi Favorito
+            </button>
+        </div>
+    </div>
+</template>
